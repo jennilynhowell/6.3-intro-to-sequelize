b7f2a44bd424c7a68c13f9bef21e41763a758c94
Jennilyn Howell <jennilynhowell@me.com>
2017-06-28 11:19:50 -0400

    webapp interfaces w db

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 const models = require('./models');
-//
-// //running this twice will generate another entry!
-// models.Movie.create({
-//   title: 'Jaws',
-//   release_date: new Date(1975, 1, 10),
-//   imdb_link: 'http://www.imdb.com'
-// });
 
-// models.Movie.findOne({
-//   where: {
-//     title: "Jaws"
-//   }
-// }).then(function(movie){
-//   console.log(movie.release_date);
-// });
+const app = express();
 
-models.Movie.create({
-  title: 'Home Alone 2: Lost in New York',
-  release_date: new Date(1998, 10, 16),
-  imdb_link: 'http://www.imdb.com'
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', './views');
+
+app.get('/', function(req, res){
+
+  models.Movie.findAll().then(function(movies){
+    res.render('index', {model: movies})
+  }); //find functions return a promise
+
 });
+
+app.listen(3000);
