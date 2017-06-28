60bb5a82a7080a481169313e11827515421ee009
Jennilyn Howell <jennilynhowell@me.com>
2017-06-28 14:03:05 -0400

    added a new column

---
+++ migrations/20170628174812-unnamed-migration.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn(
+    'Movies',
+    'genre',
+    Sequelize.STRING
+  );
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn(
+      'Movies',
+      'genre'
+    );
+    
+  }
+};
